f6f25f196b24f72dcbd49c012bbcf7e15de18e0e
brkr19 <brkr19@users.noreply.github.com>
2020-07-28 11:37:21 -0500

    Link .vimrc

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 (cd ~/.dotfiles && git submodule update --init --recursive && git submodule foreach --recursive git fetch)
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux-plugins/oh-my-tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
