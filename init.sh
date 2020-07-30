a21b34ec3ee18d60b2e1a88471608e469c412205
brkr19 <brkr19@users.noreply.github.com>
2020-07-30 10:57:46 -0500

    Force link updates and get submodules after setting gitconfig

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-(cd ~/.dotfiles && git submodule update --init --recursive && git submodule foreach --recursive git fetch)
+ln -s -f ~/.dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/.dotfiles/tmux-plugins/oh-my-tmux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -s -f ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s -f ~/.dotfiles/ssh/config ~/.ssh/config
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/tmux-plugins/oh-my-tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+(cd ~/.dotfiles && git submodule update --init --recursive && git submodule foreach --recursive git fetch)
 
 sudo chsh -s $(which zsh) $(whoami)
 chsh -s $(which zsh)
