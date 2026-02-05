40056c4373e56e14aa1f476fba81794506893d61
brkr19 <brkr19@users.noreply.github.com>
2026-02-05 11:36:11 -0600

    Remove hardcoded git user info

--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@ ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -s -f ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s -f ~/.dotfiles/ssh/config ~/.ssh/config
 
+# Create .gitconfig.local if it doesn't exist
+if [ ! -f ~/.gitconfig.local ]; then
+    echo "Creating ~/.gitconfig.local from example..."
+    cp ~/.dotfiles/.gitconfig.local.example ~/.gitconfig.local
+    echo "Please edit ~/.gitconfig.local to add your user information."
+fi
+
 (cd ~/.dotfiles && git submodule update --init --recursive && git submodule foreach --recursive git fetch)
 
 echo "Make sure to install ZSH and run 'chsh -s /usr/bin/zsh' if you haven't already done so."
